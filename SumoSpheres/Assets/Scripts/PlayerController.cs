e4190cc0d4e65175e6a7f4bc5f69493dcf48e620
Snejj <jaydenbauer35@gmail.com>
2019-01-29 17:49:27 -0500

    Added initial jump support and walls to TestScene

--- SumoSpheres/Assets/Scripts/PlayerController.cs
+++ SumoSpheres/Assets/Scripts/PlayerController.cs
@@ -6,20 +6,28 @@ using UnityEngine.Networking;
 public class PlayerController : NetworkBehaviour
 {
     public float speed;
+    public float jumpSpeed;
+    private float distanceToGound;
     private Rigidbody playerRB;
+    private SphereCollider playerCollider;
     Transform cameraTransform;
 
     public override void OnStartLocalPlayer()
     {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         // Get a reference to the local player's rigid body and change the player's material color
         // to cyan.
         playerRB = GetComponent<Rigidbody>();
+        playerCollider = GetComponent<SphereCollider>();
         GetComponent<MeshRenderer>().material.color = Color.cyan;
 
         // Set the main camera's target to the player's transform and get a reference to the main
         // camera's transform.
         Camera.main.GetComponent<ThirdPersonCamera>().target = this.transform;
         cameraTransform = Camera.main.transform;
+        distanceToGound = playerCollider.bounds.extents.y;
     }
 
     private void FixedUpdate()
@@ -29,7 +37,10 @@ public class PlayerController : NetworkBehaviour
         {
             Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             Vector2 inputDirection = input.normalized;
-
+            if(Input.GetKey(KeyCode.Space) & IsGrounded())
+            {
+                playerRB.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            }
             if(inputDirection != Vector2.zero)
             {
                 // Add force to the player dependent on input axes and camera direction.
@@ -38,5 +49,10 @@ public class PlayerController : NetworkBehaviour
                 playerRB.AddForce(actualMovement * speed);
             }
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distanceToGound + 0.1f);
     }
 }
