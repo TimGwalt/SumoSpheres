b28b08caba42e22b852085ef79e4340bd51f5a52
Snejj <jaydenbauer35@gmail.com>
2019-01-29 15:10:11 -0500

    Added very minimal documentation

--- SumoSpheres/Assets/Scripts/PlayerController.cs
+++ SumoSpheres/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,25 @@ using UnityEngine.Networking;
 public class PlayerController : NetworkBehaviour
 {
     public float speed;
-    private Rigidbody rb;
+    private Rigidbody playerRB;
     Transform cameraTransform;
 
     public override void OnStartLocalPlayer()
     {
-        rb = GetComponent<Rigidbody>();
+        // Get a reference to the local player's rigid body and change the player's material color
+        // to cyan.
+        playerRB = GetComponent<Rigidbody>();
         GetComponent<MeshRenderer>().material.color = Color.cyan;
+
+        // Set the main camera's target to the player's transform and get a reference to the main
+        // camera's transform.
         Camera.main.GetComponent<ThirdPersonCamera>().target = this.transform;
         cameraTransform = Camera.main.transform;
     }
 
     private void FixedUpdate()
     {
+        // Only allow the client to control their local player.
         if(isLocalPlayer)
         {
             Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
@@ -26,9 +32,10 @@ public class PlayerController : NetworkBehaviour
 
             if(inputDirection != Vector2.zero)
             {
+                // Add force to the player dependent on input axes and camera direction.
                 Vector3 movement = new Vector3(input.x, 0.0f, input.y);
                 Vector3 actualMovement = cameraTransform.TransformDirection(movement);
-                rb.AddForce(actualMovement * speed);
+                playerRB.AddForce(actualMovement * speed);
             }
         }
     }
--- SumoSpheres/Assets/Scripts/ThirdPersonCamera.cs
+++ SumoSpheres/Assets/Scripts/ThirdPersonCamera.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
 {
-    public float sensitivity = 10;
+    // Transform of the target the camera should look at.
     public Transform target;
+    float pitch;
+    float yaw;
+    public float sensitivity = 10;
+    // Float storing how far away the camera should be from the target.
     public float offset = 2.0f;
-    public Vector2 pitchBounds = new Vector2(-10, 85);
     [Range(0.01f, 1.0f)]
     public float smoothFactor = 0.12f;
+    // Vector containing the boundaries for the camera's pitch.
+    public Vector2 pitchBounds = new Vector2(-10, 85);
     Vector3 rotationSmoothVelocity;
     Vector3 currentRotation;
-
-    float yaw;
-    float pitch;
 
     void LateUpdate()
     {
@@ -24,9 +26,11 @@ public class ThirdPersonCamera : MonoBehaviour
             pitch -= Input.GetAxis("Mouse Y") * sensitivity;
             pitch = Mathf.Clamp(pitch, pitchBounds.x, pitchBounds.y);
 
+            // Smooth the camera rotation and rotate the camera's transform.
             currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, smoothFactor);
             transform.eulerAngles = currentRotation;
 
+            // Place the camera behind the target.
             transform.position = target.position - transform.forward * offset;
         }
     }
