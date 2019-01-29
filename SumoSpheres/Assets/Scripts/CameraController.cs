42448e5b5a22615acd4f01ca43ef3190ce4003a5
Snejj <jaydenbauer35@gmail.com>
2019-01-29 02:50:32 -0500

    Added a range slider in the inspector for smoothFactor

--- SumoSpheres/Assets/Scripts/CameraController.cs
+++ SumoSpheres/Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
 
     public Transform player;
     public Vector3 offset = new Vector3(0, 5, -5);
+
+    [Range(0.01f, 1.0f)]
     public float smoothFactor = 0.5f;
     public bool enableRotation = true;
     public float rotationSpeed = 5.0f;
@@ -24,7 +26,6 @@ public class CameraController : MonoBehaviour
             }
             Vector3 newPosition = player.position + offset;
             transform.position = Vector3.Slerp(transform.position, newPosition, smoothFactor);
-            // transform.position = player.position + offset;
             transform.LookAt(player);
         }
     }
