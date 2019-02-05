906ace9fa2bab32b0e54b027060810e7c3c84aad
Snejj <jaydenbauer35@gmail.com>
2019-02-05 02:02:07 -0500

    Added script to detect obstacles between the camera and player

---
+++ SumoSpheres/Assets/Scripts/DetectObstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObstacle : MonoBehaviour
+{
+    public Transform player;
+    private LayerMask fadeMask;
+
+    void Start()
+    {
+        fadeMask = LayerMask.GetMask("FadeObstacle");
+    }
+
+    void Update()
+    {
+        if (player)
+        {
+            RaycastHit obstructionHit;
+            if(Physics.Raycast(transform.position, player.position - transform.position, out obstructionHit, Mathf.Infinity))
+            {
+                Debug.Log(obstructionHit.transform.gameObject);
+            }
+        }
+    }
+}
