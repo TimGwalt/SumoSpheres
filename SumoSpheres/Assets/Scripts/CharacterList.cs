4df87cd780bf826b5cff6f0fe4ec2cf54a3ecb44
Tim Gwaltney <gwaltneyt15@students.ecu.edu>
2019-02-13 20:05:11 -0500

    Fixing player select
    Added 5 unique characters.
    Readded the character select menu.

---
+++ SumoSpheres/Assets/Scripts/CharacterList.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterList : MonoBehaviour
+{
+    public static GameObject[] characterList;
+    public static int index;
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = PlayerPrefs.GetInt("Character Selected");
+        //Make Array for childCount amount of characters
+        characterList = new GameObject[transform.childCount];
+
+        //Fill characterList array with child at their childCount
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            characterList[i] = transform.GetChild(i).gameObject;
+        }
+
+        foreach(GameObject go in characterList)
+        {
+            go.SetActive(false);
+        }
+
+        if (characterList[index])
+        {
+            characterList[index].SetActive(true);
+        }
+
+    }
+
+    public void ToggleLeft()
+    {   
+        //Toggle off the curretn model
+        characterList[index].SetActive(false);
+
+        //moves index to the left
+        index--;
+        
+        //if index moves too far left it cycles to the end of the list
+        if (index < 0)
+        {
+            index = characterList.Length - 1;
+        }
+        
+        //Toggle on the new model
+        characterList[index].SetActive(true);
+
+        Debug.Log("This is character: " + index);
+    }
+
+    public void Toggleright()
+    {
+        characterList[index].SetActive(false);
+
+        index++;
+
+        if(index == characterList.Length)
+        {
+            index = 0;
+        }
+
+        characterList[index].SetActive(true);
+
+        Debug.Log("This is characher: " + index);
+
+    }
+
+    public void Confirm()
+    {
+        PlayerPrefs.SetInt("CharacterSelected",index);
+        Debug.Log("You've picked character:" + index);
+        SceneManager.LoadScene("Tim_Test");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SumoSpheres/Assets/Scripts/NetworkController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+
+public class NetworkController : NetworkManager
+{
+    //One spawn point
+    public NetworkStartPosition[] List;
+    public static Transform spawnPosition;
+
+    //index of cur player from the character selection
+    int curPlayer = CharacterList.index;
+    
+
+    //Runs at the begining of every call.
+    private void Start()
+    {
+        //Debug for initial value.
+        Debug.Log(curPlayer);
+        //Intializeze the first spawn point.
+        spawnPosition = List[0].transform;
+        Debug.Log("index is: " + curPlayer + " coming into network");
+    }
+
+
+    //Called on client when connect
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+
+        // Create message to set the player
+        IntegerMessage msg = new IntegerMessage(curPlayer);
+
+        // Call Add player and pass the message
+        ClientScene.AddPlayer(conn, 0, msg);
+    }
+
+    //Adding the player to the server.
+    public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+    {
+        //Select the prefab from the spawnable objects list
+        var playerPrefab = spawnPrefabs[curPlayer];
+
+        //get spawnposition.
+        spawnPosition = UpdateSpawnPosition(spawnPosition);
+        Debug.Log("Spawning at : " + spawnPosition);
+
+        // Create player object with prefab
+        GameObject player = Instantiate(playerPrefab,spawnPosition) as GameObject;
+
+        //To make sure the correct player is being added
+        Debug.Log("character deployed as player" + curPlayer);
+
+        // Add player object for connection
+        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+    }
+
+    private Transform UpdateSpawnPosition(Transform spawn)
+    {
+
+        if(this.playerSpawnMethod == PlayerSpawnMethod.Random)
+        {
+            Debug.Log("Random");
+            return spawnPosition = List[Random.Range(0, List.Length)].transform;
+
+        }
+
+        if(this.playerSpawnMethod == PlayerSpawnMethod.RoundRobin)
+        {
+            Debug.Log("Round Robin");
+            for(int i= 0; i<= List.Length; i++)
+            {
+                if(List[i].transform.Equals(spawn.transform))
+                {
+                    //Debug.Log("Last spawn was " + List[i].transform);
+                    if(i+1 == List.Length)
+                    {
+                        //Debug.Log("restarting list");
+                        return List[0].transform;
+                    }
+                        //Debug.Log("Spawning at spawnPosition: " + List[i+1].transform);
+                        return List[i+1].transform;
+                }
+               
+            }
+
+        }
+        return spawnPosition;
+    }
+}
