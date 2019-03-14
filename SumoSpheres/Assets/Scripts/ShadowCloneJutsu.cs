d44edc62bd9e0433360511762ade8d51752b1ea7
Jamison <Brownjam15@students.ecu.edu>
2019-03-14 15:55:14 -0400

    initial code for cloning powerup

---
+++ SumoSpheres/Assets/Scripts/ShadowCloneJutsu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowCloneJutsu : MonoBehaviour
+{
+    public GameObject player;
+    GameObject PlayerClone;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown("c"))
+        {
+            PlayerClone = Instantiate(player, transform.position, Quaternion.identity) as GameObject;
+        }
+
+
+    }
+}
