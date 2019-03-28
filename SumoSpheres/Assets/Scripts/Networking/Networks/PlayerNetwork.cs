87cad14a91ef60f7ff7e066763ee9b630287cda2
Snejj <jaydenbauer35@gmail.com>
2019-03-28 00:22:39 -0400

    Added ability to create and browse lobbies

---
+++ SumoSpheres/Assets/Scripts/Networking/Networks/PlayerNetwork.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour
+{
+    public static PlayerNetwork m_Instance;
+    public string m_Name {get; private set;}
+
+    void Awake()
+    {
+        m_Instance = this;
+
+        m_Name = "Player#" + Random.Range(1000, 9999);
+    }
+}
---
+++ SumoSpheres/Assets/Scripts/Networking/RoomListing.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RoomListing : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI m_RoomNameText;
+    private TextMeshProUGUI RoomNameText
+    {
+        get { return m_RoomNameText; }
+    }
+    public string m_RoomName { get; private set; }
+
+    public bool m_Updated { get; set; }
+
+    void Start()
+    {
+        GameObject lobbyCanvasObject = JoinCanvasManager.m_Instance.LobbyCanvas.gameObject;
+        if (lobbyCanvasObject == null)
+            return;
+        
+        LobbyCanvas lobbyCanvas = lobbyCanvasObject.GetComponent<LobbyCanvas>();
+
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(() => lobbyCanvas.OnClickJoinRoom(RoomNameText.text));
+    }
+
+    void OnDestroy() {
+        Button button = GetComponent<Button>();
+        button.onClick.RemoveAllListeners();
+    }
+
+    public void SetRoomNameText(string text)
+    {
+        m_RoomName = text;
+        RoomNameText.text = m_RoomName;
+    }
+}
