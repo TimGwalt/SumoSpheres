36ddd316c52a8dc076b4309857c67c29acef3436
Jamison <Brownjam15@students.ecu.edu>
2019-04-04 12:26:14 -0400

    implement cloning ability
    now have the ability to clone character by pressing 'c'.

--- SumoSpheres/Assets/Scripts/ShadowCloneJutsu.cs
+++ SumoSpheres/Assets/Scripts/ShadowCloneJutsu.cs
@@ -6,22 +6,24 @@ public class ShadowCloneJutsu : MonoBehaviour
 {
     public GameObject player;
     GameObject PlayerClone;
-    public float coolDown;
     private float timeStamp;
-    
+    public float coolDown;
+    public float sec = 5f;
+  
     void Update()
     {
-        if(timeStamp <= Time.time) 
+        if (timeStamp <= Time.time)
         {
             if (Input.GetKeyDown("c"))
-            {
-                Debug.Log("kage bushin no justu");
-                PlayerClone = Instantiate(player, transform.position, Quaternion.identity) as GameObject;
-                timeStamp = Time.time + coolDown;
-                Destroy(PlayerClone, 3);
+            { 
+              
+                    Debug.Log("kage bushin no justu");
+                    PlayerClone = Instantiate(player, transform.position, Quaternion.identity) as GameObject;
+                    PlayerClone.GetComponent<ShadowCloneJutsu>().enabled = false;
+                    Destroy(PlayerClone, sec);
+                    timeStamp = Time.time + coolDown;
+
             }
         }
-
-    }
-
+    }             
 }
