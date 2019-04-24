cf2b4a6dc5705b077c47f7fa28e74d3806bc4a5d
scarboroughr15 <scarboroughr15@students.ecu.edu>
2019-04-23 23:41:46 -0400

    Updated the TeleportAbility script to work correctly with the parent class.

--- SumoSpheres/Assets/Scripts/Abilities/TeleportAbility.cs
+++ SumoSpheres/Assets/Scripts/Abilities/TeleportAbility.cs
@@ -6,9 +6,8 @@ public class TeleportAbility : NetworkBasePlayerMovement
 {
     public override void CheckInput()
     {
+        base.CheckInput();
         float coolDownTimer = Time.deltaTime + 5; 
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 inputDirection = input.normalized;
         if(Input.GetKey(KeyCode.E) && coolDownTimer < Time.time)
         {
             Vector3 tele = new Vector3(m_PlayerRB.position.x * -1, m_PlayerRB.position.y, m_PlayerRB.position.z * -1);
