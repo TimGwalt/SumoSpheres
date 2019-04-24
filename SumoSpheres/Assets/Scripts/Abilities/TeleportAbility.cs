b2c99f2cfa12c532841a9816f2a585e6a70525c7
Tim Gwaltney <gwaltneyt15@students.ecu.edu>
2019-04-24 10:30:24 -0400

    Fixed cool down timer on teleport ability
    Fixed cool down timer on teleport ability
    Added documentation

--- SumoSpheres/Assets/Scripts/Abilities/TeleportAbility.cs
+++ SumoSpheres/Assets/Scripts/Abilities/TeleportAbility.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 using System.Timers;
 public class TeleportAbility : NetworkBasePlayerMovement
 {
+    float coolDownTimer = Time.time + 5; 
+    
+    // Overrides the checkInput method from the base class.
+    // Changes the players loction to the opposite x and z axis coords.
     public override void CheckInput()
     {
         base.CheckInput();
-        float coolDownTimer = Time.deltaTime + 5; 
-        if(Input.GetKey(KeyCode.E) && coolDownTimer < Time.time)
-        {
-            Vector3 tele = new Vector3(m_PlayerRB.position.x * -1, m_PlayerRB.position.y, m_PlayerRB.position.z * -1);
-            m_PlayerRB.position = tele;
-            coolDownTimer = Time.deltaTime + 5; 
-        }
+       
+            if(Input.GetKeyUp(KeyCode.E) & Time.time > coolDownTimer) // Checks for of "E" input and cool down.
+            {
+                Vector3 tele = new Vector3(m_PlayerRB.position.x * -1, m_PlayerRB.position.y, m_PlayerRB.position.z * -1);
+                m_PlayerRB.position = tele;
+                coolDownTimer = Time.time + 5; 
+            }
+    }
+
+    // Updated once per frame. 
+    // Used to keep track of time for ability cool down.
+    private void update()
+    {
+        CheckInput();
     }
 }
