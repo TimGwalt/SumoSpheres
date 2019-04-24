f909474fd749ce120f20d3a60b790ac07585adeb
Snejj <jaydenbauer35@gmail.com>
2019-04-24 03:50:29 -0400

    Master room ui now hidden from non-master clients

--- SumoSpheres/Assets/Scripts/Networking/OnRoomClick.cs
+++ SumoSpheres/Assets/Scripts/Networking/OnRoomClick.cs
@@ -6,6 +6,8 @@ public class OnRoomClick : MonoBehaviour
 {
     public void DisplayRoom()
     {
+        GameObject.Find("StartMatchButton").SetActive(false);
+        GameObject.Find("RoomState").SetActive(false);
         JoinCanvasManager.m_Instance.gameObject.GetComponent<Canvas>().enabled = false;
         RoomCanvasManager.m_Instance.gameObject.GetComponent<Canvas>().enabled = true;
     }
